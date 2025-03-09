fb222261e5aac105d50cc51c9ce0d2dba9135d9a
Cal <d00269431@student.dkit.ie>
2025-03-09 20:26:05 +0000

    Final Push, finished style for productPage, refined everything else

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -8,39 +8,37 @@ export default class Home extends Component {
                 {/* Parallax Section */}
                 <div className="parallax">
                     <div className="parallax-content">
-                        <h1>COOL PARALAX EFFECT</h1>
+                        <h1>PianoVibe</h1>
                     </div>
                 </div>
 
-                {/* Navigation */}
-                <nav>
-                    <Link to="/login" className="btn btn-primary">Login</Link>
-                </nav>
-
                 {/* Cards Section */}
-                <div className="GoToProducts">
-                <div className="cards-container">
-                    <div className="card">
+                <div className="home-cards-container">
+                    <div className="home-card">
                         <Link to="/Products">
                             <img src="https://pianocentre.com/wp-content/uploads/images/products/products-yam-u1-super_c.jpg" alt="Product 1" />
                             <p>Check out our latest products!</p>
                         </Link>
                     </div>
-                    <div className="card">
+                    <div className="home-card">
                         <Link to="/Products">
                             <img src="https://www.coachhousepianos.co.uk/wp-content/uploads/2024/07/Steinway-D-2.jpg" alt="Product 2" />
                             <p>Exclusive deals available now!</p>
                         </Link>
                     </div>
-                    <div className="card">
+                    <div className="home-card">
                         <Link to="/Products">
                             <img src="https://promusica.ie/wp-content/uploads/2023/01/fp30xbk_angle_gal.jpg" alt="Product 3" />
                             <p>Shop the best quality items!</p>
                         </Link>
                     </div>
                 </div>
-                </div>
 
+                {/* About Section */}
+                <div className="about-section">
+                    <h2>Read More About Us Here</h2>
+                    <Link to="/AboutUs" className="about-link">Learn More</Link>
+                </div>
             </div>
         );
     }
